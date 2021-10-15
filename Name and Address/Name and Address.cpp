853c1ff46f7a50186a8e2a2ea93869a44d56093b
Nikita Polunin (二木立・半人) <Stkchanelx@gmail.com>
2021-10-15 16:03:02 +0300

    Добавьте файлы проекта.

---
+++ Name and Address/Name and Address.cpp
@@ -0,0 +1,8 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Name: Nikita." << endl << "Address: Volgograd city, Sovetsky District, Pushkin Street, Kolotushkin House 2 " << endl;
+}
